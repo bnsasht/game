47239b3cee1e34deafce7ba05a15bb3fbe442858
sarumoktan <sarumoktan198@gmail.com>
2026-06-12 11:49:50 +0545

    level 1 of genz added

--- themes/genz.js
+++ themes/genz.js
@@ -3,9 +3,17 @@ const theme_genz = {
   name: 'Gen Z Protest',
   emoji: '✊',
   description: 'Placards, paint, and people power',
-  
-      
-    },
+  premium: true,
+  price: '$2.49',
+  coinPrice: 300,
+  levels: [
+    // paste your 5 genz levels here 
 
-    //level 2
-    
+    {
+      name: 'GenZ for Change',
+      difficulty: 'Easy',
+     
+       
+      
+    }  ]
+};
