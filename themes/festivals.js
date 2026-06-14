96832e9b62321831e0d10e4b123622f10346b9d5
d9ya <diya26162@gmail.com>
2026-06-14 08:36:41 +0545

    festivals level1 added

--- themes/festivals.js
+++ themes/festivals.js
@@ -5,6 +5,27 @@ const theme_festivals = {
   description: 'Masks, chariots, and festival chaos',
   premium: false,
   levels: [
-    // paste your 5 festival levels here 
+    
+
+  //code for level 1 of Festivals theme
+    {
+      name: 'Bisket Jatra',
+      difficulty: 'Easy',
+      timeLimit: 60,
+      differences: 6,
+      coinsPerFind: 5,
+      imgLeft:  'images/himalaya/bisketjatra_level1_first.jpeg',
+      imgRight: 'images/himalaya/bisketjatra_level1_second.jpeg',
+      hotspots: [
+        { x: 0.499, y: 0.156, r: 0.06 },
+ { x: 0.501, y: 0.265, r: 0.06 },
+ { x: 0.776, y: 0.453, r: 0.06 },
+ { x: 0.315, y: 0.811, r: 0.06 },
+ { x: 0.323, y: 0.711, r: 0.06 },
+ { x: 0.554, y: 0.807, r: 0.06 },
+ { x: 0.349, y: 0.409, r: 0.06 },
+      ]
+    },
   ]
 };
+
